def44a0d7b59e4013f213f2bc8240370a4ffd596
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-05-13 18:41:35 +0530

    added corsconfig

--- src/main/java/com/ecommerceapp/UserappApiApplication.java
+++ src/main/java/com/ecommerceapp/UserappApiApplication.java
@@ -2,6 +2,9 @@ package com.ecommerceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserappApiApplication {
@@ -9,5 +12,15 @@ public class UserappApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserappApiApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	 return new WebMvcConfigurer() {
+	   @Override
+	   public void addCorsMappings(CorsRegistry registry) {
+	     registry.addMapping("/**").allowedOrigins("*");
+	   }
+	 };
+	}
+	
 }

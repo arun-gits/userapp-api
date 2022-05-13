90c28228260e46d8a18150a9f2abc447ede8eed5
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-05-13 20:13:33 +0530

    added corsconfig

--- src/main/java/com/ecommerceapp/UserappApiApplication.java
+++ src/main/java/com/ecommerceapp/UserappApiApplication.java
@@ -2,9 +2,6 @@ package com.ecommerceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserappApiApplication {
@@ -12,13 +9,5 @@ public class UserappApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserappApiApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-	 return new WebMvcConfigurer() {
-	   @Override
-	   public void addCorsMappings(CorsRegistry registry) {
-	     registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedMethods("*");
-	   }
-	 };
-	}
+
 }
--- src/main/java/com/ecommerceapp/config/CorsConfiguration.java
+++ src/main/java/com/ecommerceapp/config/CorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class CorsConfiguration extends WebMvcConfigurationSupport {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("http://localhost:4200/");
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedMethods("*");
 		// .allowCredentials(true);
 	}
 }

dbd59834a9b5889b1d4368a3f6133ee9c6f58ed7
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-05-17 14:12:55 +0530

    added make as admin feature

--- src/main/java/com/ecommerceapp/controller/UserController.java
+++ src/main/java/com/ecommerceapp/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -76,7 +75,7 @@ public class UserController {
 		return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
 	}
 	}
-	@PatchMapping("makeasadmin/{id}")
+	@GetMapping("makeasadmin/{id}")
 	public ResponseEntity<?> makeAsAdmin(@PathVariable("id") int id){
 		try {
 		userService.makeAsAdmin(id);
